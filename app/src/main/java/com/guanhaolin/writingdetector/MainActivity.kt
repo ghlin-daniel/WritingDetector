2fc3d27c4eade5af12aaeb0536b9065af94caad6
Daniel Lin <ghlin.daniel@gmail.com>
2025-08-17 21:31:11 -0700

    Add top bar

--- app/src/main/java/com/guanhaolin/writingdetector/MainActivity.kt
+++ app/src/main/java/com/guanhaolin/writingdetector/MainActivity.kt
@@ -7,17 +7,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
 import com.guanhaolin.writingdetector.ui.theme.WritingdetectorTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             WritingdetectorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { padding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(title = { Text("Writing Detector") }, actions = {
+                            IconButton(onClick = {}) {
+                                Icon(
+                                    Icons.Filled.Delete,
+                                    contentDescription = "Delete"
+                                )
+                            }
+                        })
+                    }
+                ) { padding ->
                     Box(
                         modifier = Modifier
                             .fillMaxSize()
